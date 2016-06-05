4d55f9dfd18de1da1533ae8f2136e1815289be37
qloog <quanlongwang@gmail.com>
2016-06-05 22:03:02 +0800

    modify log

--- application/library/Core/Support/Log.php
+++ application/library/Core/Support/Log.php
@@ -1,16 +1,14 @@
 <?php
 
-namespace Support;
+namespace Core\Support;
 
-use Monolog\Handler\ErrorLogHandler;
-use Monolog\Handler\StreamHandler;
-use Monolog\Handler\NullHandler;
+use Yaf\Registry;
 use Monolog\Logger;
-use Psr\Log\LoggerInterface;
+use Monolog\Handler\NullHandler;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\SyslogUdpHandler;
+use Monolog\Formatter\LineFormatter;
 
-/**
- * Class Log.
- */
 class Log
 {
     /**
@@ -19,6 +17,9 @@ class Log
      * @var \Psr\Log\LoggerInterface
      */
     protected static $logger;
+
+    protected static $channelName = 'yaf.admin';
+
     /**
      * Return the logger instance.
      *
@@ -26,17 +27,19 @@ class Log
      */
     public static function getLogger()
     {
-        return self::$logger ?: self::$logger = self::createDefaultLogger();
+        return self::$logger[self::$channelName] ?: self::$logger[self::$channelName] = self::createDefaultLogger();
     }
+
     /**
      * Set logger.
      *
-     * @param \Psr\Log\LoggerInterface $logger
+     * @param LoggerInterface|\Psr\Log\LoggerInterface $logger
      */
     public static function setLogger(LoggerInterface $logger)
     {
-        self::$logger = $logger;
+        self::$logger[self::$channelName] = $logger;
     }
+
     /**
      * Tests if logger exists.
      *
@@ -44,8 +47,9 @@ class Log
      */
     public static function hasLogger()
     {
-        return self::$logger ? true : false;
+        return self::$logger[self::$channelName] ? true : false;
     }
+
     /**
      * Forward call.
      *
@@ -58,31 +62,52 @@ class Log
     {
         return forward_static_call_array([self::getLogger(), $method], $args);
     }
-    /**
-     * Forward call.
-     *
-     * @param string $method
-     * @param array  $args
-     *
-     * @return mixed
-     */
-    public function __call($method, $args)
-    {
-        return call_user_func_array([self::getLogger(), $method], $args);
-    }
+
     /**
      * Make a default log instance.
      *
-     * @return \Monolog\Logger
+     * @return Logger
      */
     private static function createDefaultLogger()
     {
-        $log = new Logger('yaf-api');
-        if (defined('PHPUNIT_RUNNING')) {
-            $log->pushHandler(new NullHandler());
-        } else {
-            $log->pushHandler(new ErrorLogHandler());
+        $config = Registry::get('config');
+        $logger = new Logger($config['log']['channel_name'] ?: self::$channelName);
+
+        if (!$config['application']['debug'] || defined('PHPUNIT_RUNNING')) {
+            $logger->pushHandler(new NullHandler());
+        } elseif ($logFile = $config['log']['directory']) {
+            switch ($config['log']['type']) {
+                case 'syslog' :
+                    $requestId = self::getRequestId();
+                    $handler = new SyslogUdpHandler(
+                        $config['host'],
+                        $config['port'],
+                        $facility = LOG_USER,
+                        $level = 'debug'
+                    );
+                    $handler->setFormatter(
+                        new LineFormatter(
+                            '[%datetime%] | %channel% | ' . $requestId . ' | %level_name% | %message% | %context%'
+                        )
+                    );
+                    break;
+                case 'file':
+                default:
+                    $handler = new StreamHandler($logFile . date('Y-m-d') . '.log', $config['log']['level']);
+            }
+            $logger->pushHandler($handler);
         }
-        return $log;
+
+        return $logger;
+    }
+
+    /**
+     * Get Request ID
+     *
+     * @return string
+     */
+    private static function getRequestId()
+    {
+        return isset($_SERVER['HTTP_X_REQUEST_ID']) ? $_SERVER['HTTP_X_REQUEST_ID'] : '-';
     }
 }
