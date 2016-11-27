461a7dbbcd154bd52b3e5ade1b11b24eafd4f02b
qloog <quanlongwang@gmail.com>
2016-11-27 23:12:01 +0800

    加入异常处理说明

--- README.md
+++ README.md
@@ -189,6 +189,12 @@
     }
 ```
 
+### 异常和错误
+
+如果配置文件中`appliation.dispatcher.throwException` 设为1或true,Yaf会抛异常, 否则则会触发错误。
+当Yaf遇到未捕获异常的时候, 就会把运行权限, 交给当前模块的Error Controller的Error Action动作, 而异常或作为请求的一个参数, 传递给Error Action.
+错误页默认在`application/views/error/error.phtml`, 可以进行自定义处理。
+
 ## yaf 中文手册
 
  - http://php.net/manual/zh/book.yaf.php
--- application/controllers/Error.php
+++ application/controllers/Error.php
@@ -1,74 +1,68 @@
 <?php
 
+use Core\Controllers\Web;
+use Core\Log;
 /**
  * yaf 框架报错类调用
- *  默认错误会调用这个Controller 中 ErrorAction
+ * 默认错误会调用这个Controller 中 ErrorAction
  */
-class ErrorController extends \Yaf\Controller_Abstract
+class ErrorController extends Web
 {
-    private $_config;
-
-    public function init()
-    {
-        $this->_config = Yaf\Application::app()->getConfig();
-    }
 
     /**
-     * [具体错误处理]
-     * @param  Exception $exception [description]
-     * @return [type]               [description]
+     * @param Exception $exception
      */
     public function errorAction(Exception $exception)
     {
-        dd($exception);
-        //\Yaf\Dispatcher::getInstance()->autoRender(false);
-        //if ($this->_config->application->showErrors) {
-        //    switch ($exception->getCode()) {
-        //        //case YAF_ERR_AUTOLOAD_FAILED:
-        //        //case YAF_ERR_NOTFOUND_MODULE:
-        //        //case YAF_ERR_NOTFOUND_CONTROLLER:
-        //        //case YAF_ERR_NOTFOUND_ACTION:
-        //        //case YAF_ERR_NOTFOUND_VIEW:
-        //        //    if (strpos($this->getRequest()->getRequestUri(), '.css') !== false ||
-        //        //        strpos($this->getRequest()->getRequestUri(), '.jpg') !== false ||
-        //        //        strpos($this->getRequest()->getRequestUri(), '.js') !== false ||
-        //        //        strpos($this->getRequest()->getRequestUri(), '.png') !== false ||
-        //        //        strpos($this->getRequest()->getRequestUri(), '.ico') !== false ||
-        //        //        strpos($this->getRequest()->getRequestUri(), '.gif') !== false
-        //        //    ) {
-        //        //        header('HTTP/1.1 404 Not Found');
-        //        //    }
-        //        default:
-        //            //记录错误日志
-        //            Log::error('error',$exception->getMessage() . ' IN FILE ' . $exception->getFile() . ' ON LINE ' . $exception->getLine());
-        //            //显示错误信息
-        //            $this->_view->exception =  $exception;
-        //            echo $this->getView()->render(APP_PATH.'/views/error/error.html');
-        //    }
-        //} else {
-        //    //禁止输出视图内容
-        //    Yaf\Dispatcher::getInstance()->enableView();
-        //    switch ($exception->getCode()) {
-        //        case YAF_ERR_AUTOLOAD_FAILED:
-        //        case YAF_ERR_NOTFOUND_MODULE:
-        //        case YAF_ERR_NOTFOUND_CONTROLLER:
-        //        case YAF_ERR_NOTFOUND_ACTION:
-        //        case YAF_ERR_NOTFOUND_VIEW:
-        //            header('HTTP/1.1 404 Not Found');
-        //            //记录日志
-        //            Log::error('error',$exception->getMessage() . ' IN FILE ' . $exception->getFile());
-        //            $this->_view->type='err404';
-        //            $this->_view->display(APP_PATH.'/views/404.php');
-        //            break;
-        //        default:
-        //            header('HTTP/1.1 500 Internal Server Error');
-        //            //记录文件错误日志
-        //            Log::error('error',$exception->getMessage() . ' IN FILE ' . $exception->getFile() . ' ON LINE ' . $exception->getLine());
-        //            //记录sentry错误日志
-        //            $this->_view->type='error';
-        //            $this->_view->display(APP_PATH.'/views/404.php');
-        //            break;
-        //    }
-        //}
+        $this->_view->setScriptPath(APP_PATH . '/views');
+
+        switch ($exception->getCode()) {
+            case YAF\ERR\AUTOLOAD\FAILED:
+            case YAF\ERR\NOTFOUND\MODULE:
+            case YAF\ERR\NOTFOUND\CONTROLLER:
+            case YAF\ERR\NOTFOUND\ACTION:
+            case YAF\ERR\NOTFOUND\VIEW:
+                if (strpos($this->getRequest()->getRequestUri(), '.css') !== false ||
+                    strpos($this->getRequest()->getRequestUri(), '.jpg') !== false ||
+                    strpos($this->getRequest()->getRequestUri(), '.js') !== false ||
+                    strpos($this->getRequest()->getRequestUri(), '.png') !== false ||
+                    strpos($this->getRequest()->getRequestUri(), '.ico') !== false ||
+                    strpos($this->getRequest()->getRequestUri(), '.gif') !== false
+                ) {
+                    header('HTTP/1.1 404 Not Found');
+                }
+                $data = [
+                    'type' => '404',
+                    'message' => 'Not Found!',
+                    'debug' => '',
+                ];
+                break;
+            default:
+                //记录错误日志
+                Log::error(
+                    $exception->getMessage() . ' IN FILE ' . $exception->getFile() . ' ON LINE ' . $exception->getLine(),
+                    [$exception->getTraceAsString()]
+                );
+
+                $data = [
+                    'type' => 'error',
+                    'message' => '网络错误,请稍候再试！',
+                    'debug' => '',
+                ];
+
+                if (
+                    $exception instanceof \Core\Exceptions\ArgumentException
+                    || $exception instanceof \Core\Exceptions\AuthException
+                    || $exception instanceof \Core\Exceptions\HttpException
+                ) {
+                    $data['message'] = $exception->getMessage();
+                }
+
+                if (isset($this->config['application']['showErrors']) && $this->config['application']['showErrors']) {
+                    $data['message'] = $exception->getMessage();
+                    $data['debug'] = print_r($exception, true);
+                }
+        }
+        $this->display('error', $data);
     }
 }
