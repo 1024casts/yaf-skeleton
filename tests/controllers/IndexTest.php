f861c260f1b7d7a26a491a2edc4bf35ed141794d
qloog <quanlongwang@gmail.com>
2017-07-07 21:11:54 +0800

    use base class and new add file

--- tests/controllers/IndexTest.php
+++ tests/controllers/IndexTest.php
@@ -5,15 +5,16 @@ use Core\Tests\PHPUnit\ControllerTestCase;
 /**
  * 首页控制器测试类
  */
-class IndexTest extends PHPUnit_Framework_TestCase
+class IndexTest extends ControllerTestCase
 {
     /**
      * 测试index方法
      */
     public function testIndex()
     {
-        $request = new \Yaf\Request\Simple("CLI", "", "Index", 'test');
-        $response = \Yaf\Application::app()->getDispatcher()->returnResponse(true)->dispatch($request);
+        $request = new \Yaf\Request\Simple("CLI", "Index", "Index", 'test');
+
+        $response = $this->_application->getDispatcher()->returnResponse(true)->dispatch($request);
 
         $content = $response->getBody();
 
