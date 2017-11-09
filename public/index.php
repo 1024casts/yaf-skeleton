267d7d4af55140088d82e207ec1fc42e60f5753e
qloog <quanlongwang@gmail.com>
2017-11-09 23:52:29 +0800

    修改cache路径

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 define('APP_ROOT', dirname(__DIR__));
 define('APP_PATH', APP_ROOT . '/application');
 define('APP_CONFIG_PATH', APP_ROOT . '/conf');
-define('STORAGE_PATH', APP_PATH . '/storage');
+define('STORAGE_PATH', APP_ROOT . '/storage');
 
 $application = new Yaf\Application(APP_ROOT . "/conf/application.ini", \Yaf\ENVIRON);
 $application->bootstrap()->run();
