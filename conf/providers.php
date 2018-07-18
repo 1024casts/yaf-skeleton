1680918a0755abaab5e750ef537ab17eecbffe29
qloog <quanlongwang@gmail.com>
2018-07-18 16:05:42 +0800

    remove repeat config

--- conf/providers.php
+++ conf/providers.php
@@ -1,45 +1,12 @@
 <?php
 
+/**
+ * ServiceProvider 可以在 app/Providers中新增，也可以通过在此配置文件中添加
+ */
 return [
     /** ====================must setting, being used everywhere==================== */
 
-    // global events manager for whole system
-    'eventsManager' => PHPCasts\Yaf\Events\Manager::class,
-
-    'config' => function () {
-        return \Yaf\Registry::get('config');
-    },
-
-    'logger' => function () {
-        // @todo more compatible
-        if (strtolower(Yaf\Dispatcher::getInstance()->getRequest()->getModuleName()) == 'console'
-            && getenv('LOG_TO_CONSOLE')
-        ) {
-            return new Monolog\Logger('console-name', [new Monolog\Handler\StreamHandler('php://output')]);
-        } else {
-            return new PHPCasts\Yaf\Log\LoggerWrapper();
-        }
-    },
-
     'sessionBag' => function () {
         return new PHPCasts\Yaf\Caches\Memory();
-    },
-    'session' => function () {
-        return \Yaf\Session::getInstance();
-    },
-
-    /** ==============================custom setting============================== */
-
-    'cache' => function ($c) {
-        return new PHPCasts\Yaf\Caches\Redis($c['redis']);
-    },
-
-    'redis' => function ($c) {
-        $config = $c['config']['redis']['default'];
-
-        $redis = new \Redis();
-        $redis->connect($config['host'], $config['port'], $config['timeout']);
-
-        return $redis;
     },
 ];
