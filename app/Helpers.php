c93a7a09fe9f5c27c758038eb4f146ac6a37b584
qloog <quanlongwang@gmail.com>
2018-07-19 15:32:51 +0800

    add comment

--- app/Helpers.php
+++ app/Helpers.php
@@ -1,20 +1,5 @@
 <?php
 
-use Yaf\Registry;
-
-if (!function_exists('app')) {
-    /**
-     * @param $make
-     * @return \PHPCasts\Yaf\ServiceContainer
-     */
-    function app($make)
-    {
-        $container = Registry::get('container');
-
-        if (!isset($container[$make])) {
-            throw new InvalidArgumentException('not exist in container');
-        }
-
-        return $container[$make];
-    }
-}
+/**
+ * 自定义业务相关的函数
+ */
